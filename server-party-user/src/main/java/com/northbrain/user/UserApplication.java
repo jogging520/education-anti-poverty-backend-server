06f31cd289b614ea594f0ac73dfc27cd684388e3
maying <13909314488@139.com>
2018-08-01 10:17:37 +0800

    MY-DEV-SCHOOL

--- server-party-user/src/main/java/com/northbrain/user/UserApplication.java
+++ server-party-user/src/main/java/com/northbrain/user/UserApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ServerPartyUserApplication {
+public class UserApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerPartyUserApplication.class, args);
+		SpringApplication.run(UserApplication.class, args);
 	}
 }
--- server-party-user/src/test/java/com/northbrain/user/UserApplicationTests.java
+++ server-party-user/src/test/java/com/northbrain/user/UserApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ServerPartyUserApplicationTests {
+public class UserApplicationTests {
 
 	@Test
 	public void contextLoads() {
