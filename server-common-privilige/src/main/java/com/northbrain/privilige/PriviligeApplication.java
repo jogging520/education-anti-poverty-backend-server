febe1ed3dafb43d7379bc15ae3449edd844d32a4
maying <13909314488@139.com>
2018-08-02 09:06:33 +0800

    MY-DEV-PRIVILEGE

---
+++ server-common-privilige/src/main/java/com/northbrain/privilige/PriviligeApplication.java
@@ -0,0 +1,12 @@
+package com.northbrain.privilige;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriviligeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriviligeApplication.class, args);
+	}
+}
