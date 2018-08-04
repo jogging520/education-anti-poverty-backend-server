c4869cdc2b85f5af40a2dcfdbddf5def8945af0d
maying <13909314488@139.com>
2018-08-04 10:48:51 +0800

    MY-DEV-SCHOOL  add componentscan to Application

--- server-common-privilige/src/main/java/com/northbrain/privilige/PriviligeApplication.java
+++ server-common-privilige/src/main/java/com/northbrain/privilige/PriviligeApplication.java
@@ -2,8 +2,10 @@ package com.northbrain.privilige;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.northbrain")
 public class PriviligeApplication {
 
 	public static void main(String[] args) {
