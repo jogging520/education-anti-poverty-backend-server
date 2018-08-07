b9ddf2f187d73c31f0abac6da42b3e64719f4e94
wq <446734796@qq.com>
2018-08-07 16:52:26 +0800

    修改session 增加ip 验证

--- server-common-session/src/main/java/com/northbrain/session/model/Session.java
+++ server-common-session/src/main/java/com/northbrain/session/model/Session.java
@@ -1,14 +1,13 @@
 package com.northbrain.session.model;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
 import lombok.*;
 import lombok.experimental.Accessors;
-import org.springframework.data.annotation.Id;
-import org.springframework.data.mongodb.core.index.Indexed;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.NotNull;
 import java.util.Date;
-
 
 @Data
 @Accessors(chain=true)
@@ -19,31 +18,32 @@ import java.util.Date;
 @Document
 public class Session {
     @Id
-    private     String      id;                 //id号
+    private String          id;                 //id号
     @NotNull
-    private     String      type;               //类型
+    private String          type;               //类型
     @NotNull
-    private     String      appType;            //应用类型：WEB CMS APP WCT LED MNT
+    private String          appType;            //应用类型：WEB、APP、WCT、CMS、LED、MNT
     @NotNull
-    private     String      category;           //类别（企业）
+    private String          category;           //类别（企业）
     @NotNull
-    private     String      user;               //用户编号
+    private String          user;               //用户编号
     @NotNull
-    @Indexed
-    private     String      userName;           //用户名
-    private     String      mobile;             //手机号
+    private String          userName;           //用户名
+    private String          mobile;             //手机号码
     @NotNull
-    private     Date        loginTime;          //登录时间
+    private String          address;            //客户端IP
     @NotNull
-    private     Long        lifeTime;           //寿命，毫秒
+    private Date            loginTime;          //登录时间
     @NotNull
-    private     Date        createTime;         //创建时间
+    private Long            lifeTime;           //寿命，毫秒
     @NotNull
-    private     Date        timestamp;          //状态时间
+    private Date            createTime;         //创建时间
     @NotNull
-    private     String      status;             //状态
+    private Date            timestamp;          //状态时间
     @NotNull
-    private     String      serialNo;           //操作流水号
+    private String          status;             //状态
     @NotNull
-    private     String      description;        //描述
+    private String          serialNo;           //操作流水号
+    @NotNull
+    private String          description;        //描述
 }
