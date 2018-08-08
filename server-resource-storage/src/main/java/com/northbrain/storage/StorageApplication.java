d47eec1c68503fb785e42828098897cea88d0bd3
446734796@qq.com <wuqiang502>
2018-08-08 10:44:41 +0800

    修改了storage

--- server-resource-storage/src/main/java/com/northbrain/storage/StorageApplication.java
+++ server-resource-storage/src/main/java/com/northbrain/storage/StorageApplication.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@ComponentScan("com.northbrain")
+@ComponentScan({"com.northbrain"})
 @Import(FdfsClientConfig.class)
 public class StorageApplication {
 
