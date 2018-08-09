923ce351900f8a593d221febdc11618647b7e902
446734796@qq.com <wuqiang502>
2018-08-09 20:43:09 +0800

    修改bootstrap.properties

--- server-common-privilige/src/main/java/com/northbrain/privilige/repository/IRoleHistoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.northbrain.privilige.repository;
-
-import com.northbrain.privilige.model.RoleHistory;
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface IRoleHistoryRepository extends ReactiveCrudRepository<RoleHistory, String > {
-}
