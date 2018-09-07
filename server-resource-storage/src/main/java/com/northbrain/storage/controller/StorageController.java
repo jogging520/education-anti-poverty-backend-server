e7b2d940ba78adc694a308e63da0eaae60594344
446734796@qq.com <wuqiang502>
2018-09-07 19:32:40 +0800

    晚上了storage服务。

--- server-resource-storage/src/main/java/com/northbrain/storage/controller/StorageController.java
+++ server-resource-storage/src/main/java/com/northbrain/storage/controller/StorageController.java
@@ -3,14 +3,13 @@ package com.northbrain.storage.controller;
 import com.northbrain.storage.model.Constants;
 import com.northbrain.storage.model.Storage;
 import com.northbrain.storage.service.StorageService;
-import lombok.extern.java.Log;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.codec.multipart.FilePart;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@Log
 public class StorageController {
     private final StorageService storageService;
 
@@ -21,24 +20,19 @@ public class StorageController {
     /**
      * 方法：上传图片至存储服务端
      * @param serialNo 流水号
-     * @param type 文件类型
+     * @param type 类型
      * @param category 类别（企业）
-     * @param multipartFile 文件
+     * @param fileParts 文件（图片）
      * @return 存储成功的文件名（全名）
      */
-    @PostMapping(value = Constants.STORAGE_HTTP_REQUEST_MAPPING, consumes = {"multipart/form-data", "application/json"})
-    public ResponseEntity<Mono<Storage>> uploadFile(@RequestParam String serialNo,
+    @PostMapping(value = Constants.STORAGE_HTTP_REQUEST_MAPPING)
+    public ResponseEntity<Flux<Storage>> uploadFile(@RequestParam String serialNo,
                                                     @RequestParam String type,
                                                     @RequestParam String category,
-                                                    @RequestBody MultipartFile multipartFile) {
-        log.info("----------");
-        log.info(serialNo);
-        log.info(type);
-        log.info(category);
-        log.info(multipartFile.getContentType());
+                                                    @RequestPart(Constants.STORAGE_FILE_ATTRIBUTE_NAME) Flux<FilePart> fileParts) {
         return ResponseEntity.ok()
                 .body(this.storageService
-                        .createFile(serialNo, type, category, multipartFile));
+                        .createFile(serialNo, type, category, fileParts));
     }
 
     /**
