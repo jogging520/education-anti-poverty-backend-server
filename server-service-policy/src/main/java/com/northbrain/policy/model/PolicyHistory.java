d4968073cb27ca203f82b081db0c826faca76b54
446734796@qq.com <wuqiang502>
2018-11-08 17:58:02 +0800

    完善了policyService的方法。

--- server-service-policy/src/main/java/com/northbrain/policy/model/PolicyHistory.java
+++ server-service-policy/src/main/java/com/northbrain/policy/model/PolicyHistory.java
@@ -3,6 +3,7 @@ package com.northbrain.policy.model;
 import lombok.*;
 import lombok.experimental.Accessors;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.NotNull;
@@ -25,7 +26,18 @@ public class PolicyHistory {
     @NotNull
     private String                  type;               //类型
     @NotNull
+    private String[]                appTypes;           //应用类型
+    @NotNull
+    private String                  category;           //企业类型
+    @NotNull
+    @Indexed(unique = true)
     private String                  name;               //标题
+    @NotNull
+    private String                  grade;              //级别
+    @NotNull
+    private String[]                keywords;           //关键字
+    @NotNull
+    private int                     readings;           //阅读数量
     @NotNull
     private Date                    createTime;         //创建时间
     @NotNull
@@ -34,6 +46,6 @@ public class PolicyHistory {
     private String                  status;             //状态
     @NotNull
     private String                  serialNo;           //操作流水号
+    @NotNull
     private String                  description;        //内容
-
 }
